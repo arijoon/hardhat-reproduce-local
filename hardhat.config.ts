4dd1804a6694acfed97282a72207f2c3c2317948
Arman Yaraee <arman.yaraee@deversifi.com>
2023-01-11 10:02:18 +0000

    Add more packages to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@nomiclabs/hardhat-etherscan'
+import '@nomicfoundation/hardhat-chai-matchers'
+import '@typechain/hardhat'
+import 'hardhat-gas-reporter'
+import '@openzeppelin/hardhat-upgrades'
+import 'solidity-coverage'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
